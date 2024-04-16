c8b5b90bc196933ab8d72e3c935811e44c138ee8
TarsorCase <gerardgonzalez41@gmail.com>
2024-04-16 14:51:10 -0400

    Subimos un gato

--- Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382445126475aa9da6c123cb098460bf6ce19f9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d80a807e5e6f1ddf952c96ed6f7e069628cd1960")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
